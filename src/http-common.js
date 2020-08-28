c568c813bf21141cb4feeb46cb19e726f1f1b9c3
marcelocaser <marcelocaser@gmail.com>
2020-08-28 14:31:19 -0300

    "removido log"

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 
-console.log('Aqui!' + JSON.stringify(process.env));
-
 //Define a URL base da origem para consumo do servico
 export default axios.create({
   baseURL: process.env.REACT_APP_PORT_API === "" ? process.env.REACT_APP_HOST_API : `${process.env.REACT_APP_HOST_API}:${process.env.REACT_APP_PORT_API}`,
