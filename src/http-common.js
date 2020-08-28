dd69ef808e2424b9457ab087d09ee6889cc1ca7e
marcelocaser <marcelocaser@gmail.com>
2020-08-28 12:57:27 -0300

    "correcao para uso de cors de forma dinamica"

--- src/http-common.js
+++ src/http-common.js
@@ -5,8 +5,7 @@ console.log('Aqui!' + JSON.stringify(process.env));
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  //baseURL: `http://${process.env.REACT_APP_HOST_API}:${process.env.REACT_APP_PORT_API}/`,
-  baseURL: `https://${process.env.REACT_APP_HOST_API}/`,
+  baseURL: process.env.REACT_APP_PORT_API === "" ? process.env.REACT_APP_HOST_API : `${process.env.REACT_APP_HOST_API}:${process.env.REACT_APP_PORT_API}`,
   headers: {
     'Content-type': 'application/json',
   },
